7b8064bea8d3fb00bfc61d43a6386fe27994e0f3
metamn <bartus.csongor@gmail.com>
2019-08-23 17:19:04 +0300

    Adding Storybook MDX support

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,10 +1,4 @@
 import { configure } from "@storybook/react";
 
 // Load stories into the sidebar
-const req = require.context("../src", true, /.stories.js$/);
-
-function loadStories() {
-  req.keys().forEach(filename => req(filename));
-}
-
-configure(loadStories, module);
+configure(require.context("../src", true, /\.stories\.(js|mdx)$/), module);
--- src/components/Home/Home.stories.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-
-import Home from "./Home";
-import description from "./Home.md";
-
-storiesOf("Home", module).add("Overview", () => <Home />, {
-  notes: { markdown: description }
-});
