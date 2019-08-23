78bacab7c7c7d8f5c491b2464062263f56a9bc1c
metamn <bartus.csongor@gmail.com>
2019-08-23 18:32:16 +0300

    Adding documentation

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,4 @@
 import { configure } from "@storybook/react";
 
 // Load stories into the sidebar
-configure(require.context("../src", true, /\.stories\.(js|mdx)$/), module);
+configure(require.context("../src", true, /\.stories\.(mdx)$/), module);
--- src/components/LoadingData/LoadingData.stories.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-
-import LoadingData from "./LoadingData";
-import description from "./LoadingData.md";
-
-storiesOf("LoadingData", module).add("Overview", () => <LoadingData />, {
-  notes: { markdown: description }
-});
